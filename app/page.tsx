22b17bcf666bfa8e5689db4b2ad0fa11abedb221
MicKoreaYoutube <118183460+MicKoreaYoutube@users.noreply.github.com>
2024-02-08 11:36:14 +0000

    dhksfy

--- app/page.tsx
+++ app/page.tsx
@@ -3,36 +3,19 @@ import Link from "next/link"
 import { siteConfig } from "@/config/site"
 import { buttonVariants } from "@/components/ui/button"
 
+import { Ban } from "lucide-react"
+
 export default function IndexPage() {
   return (
-    <section className="container grid items-center gap-6 pb-8 pt-6 md:py-10">
-      <div className="flex max-w-[980px] flex-col items-start gap-2">
-        <h1 className="text-3xl font-extrabold leading-tight tracking-tighter md:text-4xl">
-          Beautifully designed components <br className="hidden sm:inline" />
-          built with Radix UI and Tailwind CSS.
+    <section className="container grid items-center gap-6 my-28 max-w-[1000px] text-center">
+      <div className="flex flex-col gap-2 items-center">
+        <Ban className="text-red-500" size={400}/>
+        <h1 className="text-3xl md:text-5xl font-extrabold leading-tight tracking-tighter font-KBO-Dia-Gothic_bold">
+          상명중 1학년 1반 건의함은 <br />2024년 2월 6일부로 폐쇄되었습니다.
         </h1>
-        <p className="max-w-[700px] text-lg text-muted-foreground">
-          Accessible and customizable components that you can copy and paste
-          into your apps. Free. Open Source. And Next.js 13 Ready.
+        <p className="text-lg md:text-2xl text-muted-foreground font-SUITE-Regular">
+          이용해주셔서 감사합니다.
         </p>
-      </div>
-      <div className="flex gap-4">
-        <Link
-          href={siteConfig.links.docs}
-          target="_blank"
-          rel="noreferrer"
-          className={buttonVariants()}
-        >
-          Documentation
-        </Link>
-        <Link
-          target="_blank"
-          rel="noreferrer"
-          href={siteConfig.links.github}
-          className={buttonVariants({ variant: "outline" })}
-        >
-          GitHub
-        </Link>
       </div>
     </section>
   )
